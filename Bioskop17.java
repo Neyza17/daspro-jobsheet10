00d6942937e906a1da486914c8b153249d7dc65c
Neyza17 <nesyaratuanastasya@gmail.com>
2024-11-09 13:21:08 +0700

    sixth commit

--- Bioskop17.java
+++ Bioskop17.java
@@ -26,8 +26,8 @@ public class Bioskop17 {
         }
         System.out.println("Nama penonton pada baris ke-3: ");
         
-        for (String i : penonton[2]) {
-            System.out.println(i);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
         System.out.println("Jumlah total baris: " + penonton.length);
         
