859141f66b9bf9230732b7f7428002b4dd174316
Neyza17 <nesyaratuanastasya@gmail.com>
2024-11-09 21:23:12 +0700

    third commit

--- BioskopWithScanner17.java
+++ BioskopWithScanner17.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class BioskopWithScanner17 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         int baris, kolom;
         String nama, next;
 
@@ -19,20 +20,38 @@ public class BioskopWithScanner17 {
 
             switch (pilihan) {
                 case 1:
-                    for (int i = 0; i < penonton.length; i++) {
-                        for (int j = 0; j < penonton[i].length; j++) {
-                            System.out.printf("Masukkan nama penonton di baris %d, kolom %d: ", i, j);
-                            penonton[i][j] = sc.nextLine();
+                    System.out.println("Anda memilih untuk input data penonton");
+                    System.out.print("Masukkan nama penonton: ");
+                    String namaNama = input.nextLine();
+
+                    while (true) {
+                        System.out.print("Masukkan baris (1-4): ");
+                        baris = input.nextInt();
+                        System.out.print("Masukkan kolom (1-2): ");
+                        kolom = input.nextInt();
+                        input.nextLine();
+
+                        if (baris < 1 || baris > 4 || kolom < 1 || kolom > 2) {
+                            System.out.println("Baris atau kolom tidak valid. Silahkan coba lagi.");
+                            continue;
+                        } 
+                        if (penonton[baris - 1][kolom - 1] != null) {
+                            System.out.println("Kursi yang dupilih sudah terisi oleh " + penonton[baris - 1][kolom - 1] + ". Silahkan pilih kursi.");
+                            continue;
                         }
+                        penonton[baris -1][kolom -1] = namaNama;
+                        System.out.println("Data penonton berhasil ditambahkan.");
+                        break;
                     }
                     break;
+                    
+
                 case 2:
-                    System.out.println("Daftar penontoon: ");
+                    System.out.println("Daftar penonton: ");
                     for (int i = 0; i < penonton.length; i++) {
                         for (int j = 0; j < penonton[i].length; j++) {
-                            if (penonton[i][j] != null) {
-                                System.out.println("Baris " + (i + 1) + ", kolom " + (j + 1) + ": " + penonton[i][j]);
-                            }
+                            String penontonNew = (penonton[i][j] != null) ? penonton[i][j] : "***";
+                            System.out.println("Baris " + (i + 1) + ", kolom" + (j + 1) + ": " + penontonNew);
                         }
                     }
                     break;
